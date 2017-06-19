7d40ce1ac8f0e0ff4c5e616bc87735ac33ab06f2
SirBlobman <blobmansrealm@gmail.com>
2017-06-18 21:04:15 -0400

    Main Class

---
+++ src/com/SirLinkups/kitsgui/Core.java
@@ -0,0 +1,15 @@
+package com.SirLinkups.kitsgui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
